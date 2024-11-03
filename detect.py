282a5d945e31d41160c1035b2924a2ed574e37ce
Nikolay Bryskin <nikicat@users.noreply.github.com>
2024-11-03 20:13:57 +0300

    update with ranks and progress

--- detect.py
+++ detect.py
@@ -1,43 +1,224 @@
 import sys
 import cv2
 import numpy as np
+from dataclasses import dataclass
+import click
+import logging
 
-def main(fps: int):
-    template_names = ['victory', 'defeat']
-    templates = [cv2.imread(f'gr_{name}.bmp', 0) for name in template_names]
-    width = 284
-    height = 160
-    frame_size = width * height * 3
-    threshold = 0.8
-    f = 0
-    prev_f = -100
-    while True:
+
+width = 284
+height = 160
+frame_size = width * height * 3
+
+@click.group()
+def cli():
+    logging.basicConfig(level=logging.INFO)
+
+
+@cli.command()
+@click.option('--fps', help='fps', type=float)
+@click.option('--skip', help='skip seconds', default=0., type=float)
+@click.option('--dump', help='dump images', default=None)
+def run(fps: float, skip: float, dump: str):
+    a = Analyzer(fps, skip, dump)
+    a.run()
+
+
+@cli.command()
+@click.argument('filename')
+def oneshot(filename):
+    a = Analyzer(1., 0)
+    f = Frame.from_file(filename)
+    a.oneshot(f).output()
+
+
+@dataclass
+class Frame:
+    f: np.array
+
+    @staticmethod
+    def from_stdin() -> 'Frame':
         frame_bytes = sys.stdin.buffer.read(frame_size)
-        if len(frame_bytes) != frame_size:
-            break
-        frame = (
-            np
-            .frombuffer(frame_bytes, np.uint8)
-            .reshape([height, width, 3])
-        )
-        for name, template in zip(template_names, templates):
-            gr_frame = cv2.cvtColor(frame[21:30,10:39,:], cv2.COLOR_BGR2GRAY)
-            m = cv2.matchTemplate(gr_frame, template, cv2.TM_CCOEFF_NORMED)
-            sim = cv2.minMaxLoc(m)[1]
-            if sim > threshold:
-                if f - prev_f > 10:
-                    output(name, f, frame, fps)
-                prev_f = f
+        if len(frame_bytes) == frame_size:
+            return Frame(
+                np
+                .frombuffer(frame_bytes, np.uint8)
+                .reshape([height, width, 3])
+            )
+
+    @staticmethod
+    def from_file(filename: str) -> 'Frame':
+        return Frame(cv2.imread(filename, cv2.IMREAD_COLOR)).bgr()
+
+    def result_img(self) -> 'Frame':
+        return Frame(self.f[21:30,10:39,:])
+
+    def rank_img(self) -> 'Frame':
+        return Frame(self.f[92:99,140:145,:])
+
+    def progress_img(self) -> 'Frame':
+        return Frame(self.f[128:129,70:215,:])
+
+    def gray(self) -> 'Frame':
+        return Frame(cv2.cvtColor(self.f, cv2.COLOR_BGR2GRAY))
+
+    def binary(self) -> 'Frame':
+        return Frame(b)
+
+    def inverse(self) -> 'Frame':
+        return Frame(255-self.f)
+
+    def progress_victory(self) -> int:
+        _, b = cv2.threshold(self.f, 150, 255, cv2.THRESH_BINARY)
+        return percent(np.max(cv2.findNonZero(b)[:,:,0])/self.width())
+
+    def progress_defeat(self) -> int:
+        _, b = cv2.threshold(self.f, 160, 255, cv2.THRESH_BINARY_INV)
+        return percent(np.min(cv2.findNonZero(b)[:,:,0])/self.width())
+
+    def width(self) -> int:
+        return len(self.f[0])
+
+    def progress(self, a: 'Analyzer') -> int:
+        white_mask = cv2.inRange(self.f, a.lower_white, a.upper_white)
+        green_mask = cv2.inRange(self.f, a.lower_green, a.upper_green)
+        combined_mask = cv2.bitwise_or(white_mask, green_mask)
+        right_boundary = np.max(cv2.findNonZero(combined_mask)) or 0
+        return percent(right_boundary/self.width())
+
+    def bgr(self) -> 'Frame':
+        return Frame(cv2.cvtColor(self.f, cv2.COLOR_RGB2BGR))
+
+    def save(self, filename: str):
+        cv2.imwrite(filename, self.bgr().f)
+
+    def matches(self, template) -> bool:
+        m = cv2.matchTemplate(self.f, template, cv2.TM_CCOEFF_NORMED)
+        s = cv2.minMaxLoc(m)[1]
+        return s > 0.8
+
+    def find_rank(self, ranks) -> int | None:
+        for i, rank_img in enumerate(ranks):
+            if rank_img is None:
+                continue
+            if self.matches(rank_img):
+                return i+1
+
+    def find_result(self, templates) -> str:
+        for name, template in templates.items():
+            if self.match_result(template):
+                return name
+
+    def match_result(self, template) -> bool:
+        threshold = 0.8
+        m = cv2.matchTemplate(self.f, template, cv2.TM_CCOEFF_NORMED)
+        sim = cv2.minMaxLoc(m)[1]
+        return sim > threshold
+
+
+class Video:
+    def __init__(self, filename: str):
+        self.capture = cv2.VideoCapture(filename)
+
+    def get_frame(num: int) -> Frame:
+        self.capture.set(cv2.CAP_PROP_POS_FRAMES, num)
+        _, buf = self.capture.read()
+        return Frame(np.frombuffer(buf, np.uint8).reshape([160, 284, 3])[:, :, ::-1])
+
+
+class Analyzer:
+    def __init__(self, fps: float, skip_secs: float = 0., dump: str = None):
+        self.fps = fps
+        self.dump = dump
+        self.results = load_results()
+        self.ranks = load_ranks()
+        self.skip = int(fps * skip_secs)
+        # Define the color range for white
+        self.lower_white = np.array([200, 200, 200], dtype=np.uint8)
+        self.upper_white = np.array([255, 255, 255], dtype=np.uint8)
+        # Define the color range for green
+        self.lower_green = np.array([0, 128, 0], dtype=np.uint8)
+        self.upper_green = np.array([128, 255, 128], dtype=np.uint8)
+        logging.info("skip every %d frames", self.skip)
+
+    def run(self):
+        i = -1
+        a = None
+        while True:
+            i += 1
+            f = Frame.from_stdin()
+            if a is not None and (a.frame + 10 * self.fps < i or f is None):
+                a.output()
+                if self.dump:
+                    a.f.save(f'dumps/{self.dump}-{a.timestr()}.bmp')
+                a = None
+            if f is None:
                 break
-        f += 1
+            if self.skip != 0 and i % self.skip != 1 and a is None:
+                continue
+
+            if result := self.get_result(f):
+                prev_result = None
+                prev_f = None
+                while True: 
+                    prev_result = result
+                    prev_f = f
+                    f = Frame.from_stdin()
+                    i += 1
+                    result = self.get_result(f)
+                    if result is None:
+                        a = self.analyze(prev_result, prev_f, i-1)
+                        break
+
+    def get_result(self, f: Frame) -> str:
+        return f.result_img().gray().find_result(self.results)
+
+    def analyze(self, result: str, f: Frame, frame: int) -> 'Analysis':
+        rank = f.rank_img().gray().find_rank(self.ranks)
+        progress = f.progress_img().progress(self)
+        return Analysis(self.fps, result, rank, progress, frame, f)
+
+    def oneshot(self, f: Frame) -> 'Analysis':
+        result = f.result_img().gray().find_result(self.results)
+        return self.analyze(result, f, 0)
 
 
-def output(name: str, f: int, frame: np.array, fps: float):
-    secs = int(f / fps % 60)
-    mins = int(f / fps // 60 % 60)
-    hours = int(f / fps // 3600)
-    print(f'{hours}:{mins:02d}:{secs:02d} {name}', flush=True)
+@dataclass
+class Analysis:
+    fps: float
+    result: str
+    rank: int
+    progress: int
+    frame: int
+    f: Frame
 
+    def output(self):
+        print(f'{self.timestr()}\t{self.result}\t{self.rank}\t{self.progress}%', flush=True)
+
+    def timestr(self) -> str:
+        secs = int(self.frame / self.fps % 60)
+        mins = int(self.frame / self.fps // 60 % 60)
+        hours = int(self.frame / self.fps // 3600)
+        millis = int(self.frame % self.fps * 1_000 / self.fps)
+        return f'{hours}:{mins:02d}:{secs:02d}.{millis:03d}'
+
+
+def save_rank(n_frame: int, rank: int):
+    f = get_frame(n_frame)
+    rank_img = f.get_rank_img().gray().save(f'ranks/{rank}.bmp')
+
+def sec_to_frame(sec: float) -> int:
+    return int(sec * 30)
+
+def load_ranks() -> list[np.array]:
+    return [cv2.imread(f'ranks/{rank}.bmp', 0) for rank in range(1,6)]
+
+def load_results() -> dict[str, np.array]:
+    # TODO: add draw
+    return {name: cv2.imread(f'results/{name}.bmp', 0) for name in ['victory', 'defeat']}
+
+def percent(n: float) -> int:
+    return int(n*100)
 
 if __name__ == '__main__':
-    main(float(sys.argv[1]))
+    cli()
---
+++ experiment.py
@@ -0,0 +1,335 @@
+import marimo
+
+__generated_with = "0.9.14"
+app = marimo.App()
+
+
+@app.cell
+def __():
+    import cv2
+    import ffmpeg
+    import numpy as np
+    import marimo as mo
+    from ipywidgets import interact
+    from matplotlib import pyplot as plt
+    from detect import Analyzer, Frame
+    return Analyzer, Frame, cv2, ffmpeg, interact, mo, np, plt
+
+
+@app.cell
+def __(Frame):
+    f = Frame.from_file('dumps/1:54:06.966.bmp') # zero-defeat
+    f = Frame.from_file('dumps/1:25:07.700.bmp') # defeat
+    f = Frame.from_file('dumps/1:03:54.533.bmp') # victory
+    f = Frame.from_file('dumps/defeat-t500.bmp') # defeat-t500
+    return (f,)
+
+
+@app.cell
+def __(np):
+    # Define the color range for white
+    lower_white = np.array([200, 200, 200], dtype=np.uint8)
+    upper_white = np.array([255, 255, 255], dtype=np.uint8)
+
+    # Define the color range for green
+    lower_green = np.array([0, 128, 0], dtype=np.uint8)
+    upper_green = np.array([128, 255, 128], dtype=np.uint8)
+    return lower_green, lower_white, upper_green, upper_white
+
+
+@app.cell
+def __(f, mo):
+    mo.image(f.f)
+    return
+
+
+@app.cell
+def __(cv2, f, lower_green, lower_white, upper_green, upper_white):
+    # Create masks for white and green colors
+    white_mask = cv2.inRange(f.progress_img().f, lower_white, upper_white)
+    green_mask = cv2.inRange(f.progress_img().f, lower_green, upper_green)
+    combined_mask = cv2.bitwise_or(white_mask, green_mask)
+    return combined_mask, green_mask, white_mask
+
+
+@app.cell
+def __(combined_mask, mo):
+    mo.image(combined_mask, height=10)
+    return
+
+
+@app.cell
+def __(combined_mask, cv2, np):
+    np.max(cv2.findNonZero(combined_mask))
+    return
+
+
+@app.cell
+def __(f, mo):
+    mo.image(f.progress_img().f, height=10)
+    return
+
+
+@app.cell
+def __(cv2, f, mo):
+    mo.image(cv2.threshold(f.progress_img().gray().f, 160, 255, cv2.THRESH_BINARY_INV)[1], height=10)
+    return
+
+
+@app.cell
+def __(cv2, f, mo):
+    mo.image(cv2.threshold(f.progress_img().gray().f, 150, 255, cv2.THRESH_BINARY)[1])
+    return
+
+
+app._unparsable_cell(
+    r"""
+    np.max(cv2.findNonZero(cv2.threshold(f.progress_img().gray().f, 150, 255, cv2.THRESH_BINARY)[1])
+    """,
+    name="__"
+)
+
+
+@app.cell
+def __(f):
+    f.gray().progress_defeat()
+    return
+
+
+app._unparsable_cell(
+    r"""
+    capture = cv(2.VideoCapture('in.mp4')
+    """,
+    name="__"
+)
+
+
+@app.cell
+def __(capture, cv2, np):
+    def get_frame(num: int):
+        capture.set(cv2.CAP_PROP_POS_FRAMES, num)
+        _, buf = capture.read()
+        return np.frombuffer(buf, np.uint8).reshape([160, 284, 3])[:, :, ::-1]
+
+    def sec_to_frame(sec: float):
+        return int(sec * 30)
+
+    def get_rank_img(f):
+        return f[92:99,140:145,:]
+
+    def get_progress_img(f):
+        return f[128:129,70:215,:]
+
+    def gray(img):
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    def binary(img):
+        _, b = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        return b
+
+    def rank_progres(binary_img):
+        return int(np.min(cv2.findNonZero(255-binary_img)[:,:,0])/len(binary_img[0])*100)
+
+    def save_rank(n_frame, rank):
+        f = get_frame(n_frame)
+        rank_img = gray(get_rank_img(f))
+        cv2.imwrite(f'ranks/{rank}.bmp', rank_img)
+
+    def load_ranks():
+        return [cv2.imread(f'ranks/{rank}.bmp', 0) for rank in range(1,6)]
+
+    def matches(img, template) -> bool:
+        m = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+        s = cv2.minMaxLoc(m)[1]
+        return s > 0.8
+    return (
+        binary,
+        get_frame,
+        get_progress_img,
+        get_rank_img,
+        gray,
+        load_ranks,
+        matches,
+        rank_progres,
+        save_rank,
+        sec_to_frame,
+    )
+
+
+@app.cell
+def __(load_ranks):
+    ranks = load_ranks()
+    return (ranks,)
+
+
+@app.cell
+def __(
+    binary,
+    get_frame,
+    get_progress_img,
+    get_rank_img,
+    gray,
+    matches,
+    mo,
+    rank_progres,
+    ranks,
+):
+    def find_rank(img) -> int | None:
+        for i, rank_img in enumerate(ranks):
+            if rank_img is None:
+                continue
+            if matches(img, rank_img):
+                return i
+
+    def analyze(num: int):
+        f = get_frame(num)
+        rank_img = gray(get_rank_img(f))
+        pr = rank_progres(binary(gray(get_progress_img(f))))
+        rank = find_rank(rank_img)
+        return mo.vstack([
+            mo.image(f, width=700),
+            mo.image(rank_img, width=70),
+            pr,
+            rank,
+        ])
+    return analyze, find_rank
+
+
+@app.cell
+def __(get_frame, sec_to_frame):
+    f_vict = get_frame(sec_to_frame(3600+180+52))
+    #mo.image(f_vict, width=700)
+    return (f_vict,)
+
+
+@app.cell
+def __(save_rank, sec_to_frame):
+    save_rank(sec_to_frame(3600+180+52), 2)
+    return
+
+
+@app.cell
+def __(get_frame, sec_to_frame):
+    f_defeat = get_frame(sec_to_frame(3600+25*60+7))
+    #mo.image(f_defeat, width=700)
+    return (f_defeat,)
+
+
+@app.cell
+def __(find_rank, get_frame, get_rank_img, gray, sec_to_frame):
+    #f = get_frame(sec_to_frame(3600+25*60+7))
+    #rank_img = gray(get_rank_img(f))
+    find_rank(gray(get_rank_img(get_frame(sec_to_frame(3600+35*60+7)))))
+    return
+
+
+@app.cell
+def __(analyze, sec_to_frame):
+    analyze(sec_to_frame(3600+25*60+7))
+    return
+
+
+@app.cell
+def __(analyze, sec_to_frame):
+    analyze(sec_to_frame(3600+46*60+50))
+    return
+
+
+@app.cell
+def __(f_vict, mo):
+    mo.image(f_vict[92:99,140:145,:], width=70)
+    return
+
+
+@app.cell
+def __(f_vict, get_progress_img, mo):
+    mo.image(get_progress_img(f_vict), height=40)
+    return
+
+
+@app.cell
+def __(rank_progres):
+    rank_progres()
+    return
+
+
+@app.cell
+def __(binary, f_vict, get_progress_img, gray, mo):
+    vict_pr_gray = gray(get_progress_img(f_vict))
+    vict_pr_binary = binary(vict_pr_gray)
+    mo.image(vict_pr_binary, width=700)
+    return vict_pr_binary, vict_pr_gray
+
+
+@app.cell
+def __(rank_progres, vict_pr_binary):
+    rank_progres(vict_pr_binary)
+    return
+
+
+@app.cell
+def __(f_defeat, get_progress_img, gray, mo):
+    mo.image(gray(get_progress_img(f_defeat)), width=700)
+    return
+
+
+@app.cell
+def __(binary, cv2, f_defeat, get_progress_img, gray, np):
+    defeat_pr_gray = gray(get_progress_img(f_defeat))
+    defeat_pr_binary = binary(defeat_pr_gray)
+    np.min(cv2.findNonZero(255-defeat_pr_binary)[:,:,0])/len(defeat_pr_binary[0])
+    return defeat_pr_binary, defeat_pr_gray
+
+
+@app.cell
+def __(b, cv2, gr_defeat, gr_victory, num_frames):
+    vfs = []
+    dfs = []
+    for frame in range(num_frames):
+        for arr, template in [(vfs, gr_victory), (dfs, gr_defeat)]:
+            m = cv2.matchTemplate(cv2.cvtColor(b[frame,21:30,10:39,:], cv2.COLOR_BGR2GRAY), template, cv2.TM_CCOEFF_NORMED)
+            s = cv2.minMaxLoc(m)[1]
+            if s > 0.8 and (len(arr) == 0 or frame - arr[-1] > 10):
+                arr.append(frame)
+    return arr, dfs, frame, m, s, template, vfs
+
+
+@app.cell
+def __(dfs, vfs):
+    dfs, vfs
+    return
+
+
+@app.cell
+def __(dfs, f):
+    for _f in dfs:
+        _secs = f * 2 % 60
+        _mins = f * 2 // 60 % 60
+        _hours = f * 2 // 3600
+        print(f'{_hours}:{_mins:02d}:{_secs:02d}')
+    return
+
+
+@app.cell
+def __(f, vfs):
+    for _f in vfs:
+        _secs = f * 2 % 60
+        _mins = f * 2 // 60 % 60
+        _hours = f * 2 // 3600
+        print(f'{_hours}:{_mins:02d}:{_secs:02d}')
+    return
+
+
+@app.cell
+def __(cv2):
+    cv2.imread('dumps/1:03:54.533.bmp', 1)
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
